7002b6c8fff3dafb9b0f93793c51288ab6d5913d
LugoDv <falugopersonal@gmail.com>
2025-07-09 19:04:53 +0200

    Soporte para port

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from fuzzywuzzy import fuzz
-import json
+import json ,os
 
 app = Flask(__name__)
 
@@ -27,4 +27,5 @@ def chatbot():
     return jsonify({"respuesta": mejor_respuesta})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
